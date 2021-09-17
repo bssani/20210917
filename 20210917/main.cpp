16956497bb27a8ba0c4066c776636d6935ffd4a5
최필립 <vlfflq48@naver.com>
2021-09-17 09:33:47 +0900

    First Initialize

---
+++ 20210917/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int num = 12;
+	float point = 0.123f;
+	char name = 'H';
+
+	cout << num << endl;
+	cout << point << endl;
+	cout << name << endl;
+}
